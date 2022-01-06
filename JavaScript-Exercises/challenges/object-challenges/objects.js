7199ed37f1e5e3bb446775f638315e81254a1516
unknown <dwilliams_64@yahoo.com>
2022-01-06 15:19:40 -0600

    Added more challenges to objects challenge and added more solutions

--- JavaScript-Exercises/challenges/object-challenges/objects.js
+++ JavaScript-Exercises/challenges/object-challenges/objects.js
@@ -7,8 +7,78 @@
  *
  */
 
+// Turn this
 const petsArray = [
   ["dog", "Fido"],
   ["cat", "Whiskers"],
   ["pig", "Wilbur"],
 ];
+
+// Into this
+const newPetsArray = [
+  {
+    dog: "Fido",
+  },
+  {
+    cat: "Whiskers",
+  },
+  {
+    pig: "Wilbur",
+  },
+];
+
+/**
+ * Challenge 2
+ *
+ * Using the sample campaign finance data from above, change each row (sub-array) into an equivalent objects
+ *
+ */
+
+// Turn this
+const dataArray = [
+  ["Name", "State", "Candidate", "Amount"],
+  ["John Doe", "IA", "Rep. Smithers", "$300"],
+  ["Mary Smith", "CA", "Sen. Nando", "$1,000"],
+  ["Sue Johnson", "TX", "Rep. Nguyen", "$200"],
+];
+
+// To this
+const newDataArray = [
+  {
+    Name: "John Doe",
+    State: "IA",
+    Candidate: "Rep. Smithers",
+    Amount: "$300",
+  },
+  {
+    Name: "Mary Smith",
+    State: "CA",
+    Candidate: "Sen. Nando",
+    Amount: "$1,000",
+  },
+  {
+    Name: "Sue Johnson",
+    State: "TX",
+    Candidate: "Rep. Nguyen",
+    Amount: "$200",
+  },
+];
+
+/**
+ * Challenge 3
+ *
+ * Write a function that takes a string and returns
+ * an object that has the letter as a property and the number
+ * of occurrences of the letter for the value
+ *
+ */
+
+// Turn this
+const str = "abbcccc";
+
+// To this
+const obj = {
+  a: 1,
+  b: 2,
+  c: 4,
+};
--- JavaScript-Exercises/challenges/object-challenges/solutions.js
+++ JavaScript-Exercises/challenges/object-challenges/solutions.js
@@ -21,3 +21,171 @@ const transToObj = (array) => {
 };
 
 console.log(transToObj(petsArray));
+
+// Solution 2
+const transToObj = (array) => {
+  return array.reduce((acc, prev) => {
+    acc[prev[0]] = prev[1];
+
+    return acc;
+  }, {});
+};
+
+console.log(transToObj(petsArray));
+
+// Solution 3
+const transToObj = (array) => {
+  array.forEach((elm, i) => {
+    const newObj = {};
+
+    newObj[elm[0]] = elm[1];
+
+    array[i] = newObj;
+  });
+
+  return array;
+};
+
+console.log(transToObj(petsArray));
+
+// Solution 4
+const transToObj = (array) => {
+  for (let i = 0; i < array.length; i++) {
+    const newObj = {};
+
+    newObj[array[i][0]] = array[i][1];
+
+    array[i] = newObj;
+  }
+
+  return array;
+};
+
+console.log(transToObj(petsArray));
+
+/**
+ *
+ * Challenge 2
+ *
+ */
+
+const dataArray = [
+  ["Name", "State", "Candidate", "Amount"],
+  ["John Doe", "IA", "Rep. Smithers", "$300"],
+  ["Mary Smith", "CA", "Sen. Nando", "$1,000"],
+  ["Sue Johnson", "TX", "Rep. Nguyen", "$200"],
+];
+
+// Solution 1
+const transToArrayObj = (array) => {
+  const categoryArray = array.shift();
+
+  for (let i = 0; i < array.length; i++) {
+    const tempObj = {};
+    for (let j = 0; j < categoryArray.length; j++) {
+      tempObj[categoryArray[j]] = array[i][j];
+    }
+    array[i] = tempObj;
+  }
+
+  return array;
+};
+
+console.log(transToArrayObj(dataArray));
+
+// Solution 2
+const transToArrayObj = (array) => {
+  const categoryArray = array.shift();
+
+  return array.reduce((acc, curr) => {
+    const tempObj = {};
+
+    curr.forEach((currElm, i) => {
+      tempObj[categoryArray[i]] = currElm;
+    });
+
+    acc.push(tempObj);
+
+    return acc;
+  }, []);
+};
+
+console.log(transToArrayObj(dataArray));
+
+// Solution 3
+const transToArrayObj = (array) => {
+  const categoryArray = array.shift();
+
+  return array.map((arrayElm) => {
+    const tempObj = {};
+    let i = 0;
+
+    return arrayElm.map((elm) => {
+      tempObj[categoryArray[i]] = elm;
+      i++;
+
+      return tempObj;
+    });
+  })[0];
+};
+
+console.log(transToArrayObj(dataArray));
+
+/**
+ *
+ * Challenge 3
+ *
+ */
+
+// Solution 1
+const letterCount = (str) => {
+  const countObj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    const isLetter = Object.keys(countObj).some((key) => key === str[i]);
+
+    if (isLetter) {
+      countObj[str[i]] += 1;
+    } else {
+      countObj[str[i]] = 1;
+    }
+  }
+
+  return countObj;
+};
+
+console.log(letterCount("bbafff"));
+
+// Solution 2
+const letterCount = (str) => {
+  const countObj = {};
+
+  for (let i = 0; i < str.length; i++) {
+    if (countObj[str[i]]) {
+      countObj[str[i]] += 1;
+    } else {
+      countObj[str[i]] = 1;
+    }
+  }
+
+  return countObj;
+};
+
+console.log(letterCount("bbafff"));
+
+// Solution 3
+const letterCount = (str) => {
+  const strArray = str.split("");
+
+  return strArray.reduce((acc, curr) => {
+    if (acc[curr]) {
+      acc[curr] += 1;
+      return acc;
+    } else {
+      acc[curr] = 1;
+      return acc;
+    }
+  }, {});
+};
+
+console.log(letterCount("bbafff"));
