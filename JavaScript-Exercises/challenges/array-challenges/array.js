a760ee9d442f238fa73882cf22683a2f2f08ea2b
unknown <dwilliams_64@yahoo.com>
2022-01-11 10:48:34 -0600

    added extra challenge for arrays and a solution

--- JavaScript-Exercises/challenges/array-challenges/array.js
+++ JavaScript-Exercises/challenges/array-challenges/array.js
@@ -48,3 +48,47 @@ const output = [
   [0, 8, 16, 24, 32, 40, 48, 56, 64, 72],
   [0, 9, 18, 27, 36, 45, 54, 63, 72, 81],
 ];
+
+// Challenge 4
+
+// 1. Sort presidents by last name in reverse alphabetical order.
+
+// 2. Create a full_name key for each hash and set its value to a string in "LASTNAME, FIRSTNAME" format. Then sort the collection, first by party affiliation and then by the full name field.
+
+const presidents = [
+  {
+    last_name: "Clinton",
+    first_name: "Bill",
+    party: "Democrat",
+    year_elected: 1992,
+    state_residence: "Arkansas",
+  },
+  {
+    last_name: "Obama",
+    first_name: "Barack",
+    party: "Democrat",
+    year_elected: 2008,
+    state_residence: "Illinois",
+  },
+  {
+    last_name: "Bush",
+    first_name: "George",
+    party: "Republican",
+    year_elected: 2000,
+    state_residence: "Texas",
+  },
+  {
+    last_name: "Lincoln",
+    first_name: "Abraham",
+    party: "Republican",
+    year_elected: 1860,
+    state_residence: "Illinois",
+  },
+  {
+    last_name: "Eisenhower",
+    first_name: "Dwight",
+    party: "Republican",
+    year_elected: 1952,
+    state_residence: "New York",
+  },
+];
--- JavaScript-Exercises/challenges/array-challenges/solution.js
+++ JavaScript-Exercises/challenges/array-challenges/solution.js
@@ -56,9 +56,7 @@ const createRange = (a, b) => {
 const findProduct = (range) => {
   const productsArray = [];
 
-  range.forEach((numOuter, i) => {
-    const tempArray = [];
-
+  range.forEach((_, i) => {
     productsArray[i] = [];
 
     range.forEach((numInner, j) => {
@@ -76,8 +74,7 @@ const findProduct = (range) => {
   const productsArray = [];
 
   for (let i = 0; i < range.length; i++) {
-    const tempArray = [];
-    productsArray[i] = tempArray;
+    productsArray[i] = [];
 
     for (let j = 0; j < range.length; j++) {
       productsArray[i][j] = range[j] * i;
@@ -88,3 +85,91 @@ const findProduct = (range) => {
 };
 
 console.log(findProduct(createRange(9)));
+
+/**
+ *
+ * Challenge 4
+ *
+ */
+
+const presidents = [
+  {
+    last_name: "Clinton",
+    first_name: "Bill",
+    party: "Democrat",
+    year_elected: 1992,
+    state_residence: "Arkansas",
+  },
+  {
+    last_name: "Obama",
+    first_name: "Barack",
+    party: "Democrat",
+    year_elected: 2008,
+    state_residence: "Illinois",
+  },
+  {
+    last_name: "Bush",
+    first_name: "George",
+    party: "Republican",
+    year_elected: 2000,
+    state_residence: "Texas",
+  },
+  {
+    last_name: "Lincoln",
+    first_name: "Abraham",
+    party: "Republican",
+    year_elected: 1860,
+    state_residence: "Illinois",
+  },
+  {
+    last_name: "Eisenhower",
+    first_name: "Dwight",
+    party: "Republican",
+    year_elected: 1952,
+    state_residence: "New York",
+  },
+];
+
+// 1. Solution: Sort presidents by last name in reverse alphabetical order.
+const sortArray = (array, prop, order = "standard") => {
+  array.sort((a, b) => {
+    let compare = {};
+
+    if (order === "standard") {
+      compare.negative = a[prop] < b[prop];
+      compare.positive = a[prop] > b[prop];
+    }
+
+    if (order === "reverse") {
+      compare.negative = a[prop] > b[prop];
+      compare.positive = a[prop] < b[prop];
+    }
+
+    if (compare.negative) return -1;
+
+    if (compare.positive) return 1;
+  });
+
+  return array;
+};
+
+sortArray(presidents, "last_name", "reverse");
+
+// 2. Solution: Create a full_name key for each hash and set its value to a string in "LASTNAME, FIRSTNAME" format. Then sort the collection, first by party affiliation and then by the full name field.
+
+// Create full_name key
+const createFullName = (array) => {
+  array.forEach((elm) => {
+    elm["full_name"] = `${elm["last_name"]}, ${elm["first_name"]}`;
+  });
+
+  return array;
+};
+
+const presidentsFullName = createFullName(presidents);
+
+// Sort by party affiliation
+const presidentsSortParty = sortArray(presidentsFullName, "party");
+
+// Sort by full name
+sortArray(presidentsSortParty, "full_name");
