38d9f4f3af60bb89bbc9e4312e3469f42abbaae6
PetriD <dimtri.williams.6448@gmail.com>
2024-07-13 14:50:01 -0500

    Implemented CRUD functions in graphql project (except for update)

--- Graphql/project/server.js
+++ Graphql/project/server.js
@@ -6,36 +6,102 @@ const typeDefs = `#graphql
   type Speaker {
       twitterHandle: String
       company: String
-      id: ID!,
+      id: ID,
       bio: String
       first: String
       last: String
       favorite: Boolean
   }
 
+  input SpeakerInput {
+    twitterHandle: String
+    company: String
+    bio: String
+    first: String!
+    last: String!
+    favorite: Boolean
+  }
+
   type Query {
     speakers: [Speaker]
     speaker(speakerId: ID!): Speaker
+  }
+
+  type Mutation {
+    addSpeaker(speakerInput: SpeakerInput!): Speaker
+    deleteSpeaker(speakerId: ID!): Speaker
+    toggleFavSpeaker(speakerId: ID!): Speaker
+   
   }
 `;
 
 const getAllSpeakers = () => axios.get("http://localhost:5000/speakers");
 
+const getSpeaker = (speakerId) =>
+  axios.get(`http://localhost:5000/speakers/${speakerId}`);
+
 const resolvers = {
   Query: {
     speakers: async (parent, args, context, info) => {
-      const response = await getAllSpeakers();
+      const { data: speakers } = await getAllSpeakers();
 
-      return response.data;
+      return speakers;
     },
     speaker: async (parent, args, context, info) => {
       const { speakerId } = args;
 
-      const response = await axios.get(
-        `http://localhost:5000/speakers/${speakerId}`
+      const { data: speaker } = await getSpeaker(speakerId);
+
+      return speaker;
+    },
+  },
+  Mutation: {
+    addSpeaker: async (parent, args, context, info) => {
+      const { speakerInput } = args;
+
+      const { first, last } = speakerInput;
+
+      const { data: speakers } = await getAllSpeakers();
+
+      const speakerExist = speakers.find(
+        (speaker) => speaker.first === first && speaker.last === last
       );
 
-      return response.data;
+      if (speakerExist) {
+        throw new Error("User already exists");
+      }
+
+      const newSpeaker = {
+        ...speakerInput,
+        favorite: speakerInput?.favorite || false,
+      };
+
+      axios.post("http://localhost:5000/speakers", newSpeaker);
+
+      return newSpeaker;
+    },
+    deleteSpeaker: async (parent, args, context, info) => {
+      const { speakerId } = args;
+
+      const speaker = await getSpeaker(speakerId);
+
+      axios.delete(`http://localhost:5000/speakers/${speakerId}`);
+
+      return speaker;
+    },
+    toggleFavSpeaker: async (parent, args, context, info) => {
+      const { speakerId } = args;
+
+      const { data: speaker } = await getSpeaker(speakerId);
+
+      const updatedSpeaker = {
+        ...speaker,
+        favorite: !speaker.favorite,
+      };
+
+      axios.put(`http://localhost:5000/speakers/${speakerId}`, updatedSpeaker);
+
+      return updatedSpeaker;
     },
   },
 };
---
+++ Graphql/project/tasks.md
@@ -0,0 +1,3 @@
+* Add an updateSpeaker mutation to update a speaker
+* When querying for a user that has been deleted or does not exist in the DB axios returns a 404 error.
+    * If user cannot be found return a message telling the client user cannot be found.
