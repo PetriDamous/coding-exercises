dfbebf66c1f6e592cc184d922317bff7fde8f25d
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-08-11 16:09:04 -0500

    Created reusable SpeakerModal.
    * Currently only used for adding speaker but will be updated in future to also update existing speaker.

--- Graphql/project/client/src/components/Toolbar.jsx
+++ Graphql/project/client/src/components/Toolbar.jsx
@@ -1,66 +1,18 @@
 import { useState } from "react";
-import { useMutation } from "@apollo/client";
-import {
-  Button,
-  Form,
-  FormGroup,
-  Input,
-  Label,
-  Modal,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-} from "reactstrap";
-import { ADD_SPEAKERS } from "../graphql/mutation";
-import { GET_SPEAKERS } from "../graphql/query";
+import { Button } from "reactstrap";
 import { currentThemeVar } from "../graphql/useApollo";
+import SpeakerModal from "./SpeakerModal";
 
 const Toolbar = () => {
   const [modal, setModal] = useState(false);
-  const [addSpeaker] = useMutation(ADD_SPEAKERS);
 
   const toggle = () => {
     setModal(!modal);
   };
 
-  const [first, setFirst] = useState("");
-  const [last, setLast] = useState("");
-  const [favorite, setFavorite] = useState(false);
-
-  const insertSpeakerEvent = (first, last, favorite) => {
-    addSpeaker({
-      variables: {
-        input: {
-          first,
-          last,
-          favorite,
-        },
-      },
-      update(cache, { data: { addSpeaker } }) {
-        const { speakers } = cache.readQuery({ query: GET_SPEAKERS });
-
-        cache.writeQuery({
-          query: GET_SPEAKERS,
-          data: {
-            speakers: [...speakers, addSpeaker],
-          },
-        });
-      },
-    });
-  };
-
   const currentTheme = currentThemeVar();
 
   const themeBtnName = currentTheme === "dark" ? "Dark" : "Light";
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    insertSpeakerEvent(first, last, favorite);
-    setFirst("");
-    setLast("");
-    setFavorite(false);
-    setModal(!modal);
-  };
 
   const handleTheme = () => {
     const currentTheme = currentThemeVar();
@@ -84,41 +36,7 @@ const Toolbar = () => {
               <Button color="info" onClick={toggle}>
                 <span>Insert Speaker</span>
               </Button>
-              <Modal isOpen={modal} toggle={toggle}>
-                <Form onSubmit={handleSubmit}>
-                  <ModalHeader toggle={toggle}>
-                    Insert Speaker Dialog
-                  </ModalHeader>
-                  <ModalBody>
-                    <FormGroup>
-                      <Label for="first">First Name</Label>{" "}
-                      <Input
-                        name="first"
-                        onChange={(e) => setFirst(e.target.value)}
-                      />
-                    </FormGroup>
-                    <FormGroup>
-                      <Label for="first">Last Name</Label>
-                      <Input
-                        name="first"
-                        onChange={(e) => setLast(e.target.value)}
-                      />
-                    </FormGroup>
-                    <FormGroup check>
-                      <Label check>
-                        <Input
-                          type="checkbox"
-                          onChange={(e) => setFavorite(e.target.value === "on")}
-                        />
-                        Favorite
-                      </Label>
-                    </FormGroup>
-                  </ModalBody>
-                  <ModalFooter>
-                    <Button type="submit">Save</Button>
-                  </ModalFooter>
-                </Form>
-              </Modal>
+              <SpeakerModal modal={modal} setModal={setModal} toggle={toggle} />
             </div>
           </li>
         </ul>
