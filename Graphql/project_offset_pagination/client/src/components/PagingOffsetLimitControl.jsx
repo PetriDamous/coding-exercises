e1a2e801f5a76abed55e3bac59a61b042d6a1500
Dimitri Williams <dimtri.williams.6448@gmail.com>
2024-08-13 17:42:54 -0500

    Finished off offset pagination.

--- Graphql/project_offset_pagination/client/src/components/PagingOffsetLimitControl.jsx
+++ Graphql/project_offset_pagination/client/src/components/PagingOffsetLimitControl.jsx
@@ -4,11 +4,13 @@ import { useReactiveVar } from "@apollo/client";
 const PagingOffsetLimitControl = () => {
   const paginationData = useReactiveVar(paginationDataVar);
 
-  console.log(paginationData);
-
   const { currentPage, totalItemCount, limit } = paginationData;
 
   const lastPage = Math.trunc((totalItemCount - 1) / limit);
+
+  const onFirst = () => {
+    paginationDataVar({ ...paginationData, currentPage: 0 });
+  };
 
   const onNext = () => {
     if (currentPage < lastPage) {
@@ -16,19 +18,34 @@ const PagingOffsetLimitControl = () => {
     }
   };
 
+  const onPrevious = () => {
+    if (currentPage > 0) {
+      paginationDataVar({ ...paginationData, currentPage: currentPage - 1 });
+    }
+  };
+
+  const onLast = () => {
+    paginationDataVar({ ...paginationData, currentPage: lastPage });
+  };
+
   return (
     <div className="pagination">
-      <a className="page-link" href="#" aria-label="First">
+      <a className="page-link" href="#" aria-label="First" onClick={onFirst}>
         <i className="fa fa-angle-double-left" aria-hidden="true"></i>
       </a>
-      <a className="page-link" href="#" aria-label="Previous">
+      <a
+        className="page-link"
+        href="#"
+        aria-label="Previous"
+        onClick={onPrevious}
+      >
         <i className="fa fa-angle-left" aria-hidden="true"></i>
       </a>
       &nbsp;{currentPage + 1}&nbsp;
       <a className="page-link" href="#" aria-label="Next" onClick={onNext}>
         <i className="fa fa-angle-right" aria-hidden="true"></i>
       </a>
-      <a className="page-link" href="#" aria-label="Last">
+      <a className="page-link" href="#" aria-label="Last" onClick={onLast}>
         <i className="fa fa-angle-double-right" aria-hidden="true"></i>
       </a>
     </div>
