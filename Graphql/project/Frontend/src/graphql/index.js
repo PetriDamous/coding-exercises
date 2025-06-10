6878c5ce9f17af0fbbcc70df0650fc121b9fbb97
Petri <dimtri.williams.6448@gmail.com>
2025-06-10 16:51:17 -0500

    Apollo Performance: Created aggregator file for mutation exports.

--- Graphql/project/Frontend/src/graphql/index.js
+++ Graphql/project/Frontend/src/graphql/index.js
@@ -1,1 +1,4 @@
-export { GET_SPEAKERS } from "./speakers.js";
+export { GET_SPEAKERS } from "./queries.js";
+export { TOGGLE_SPEAKER_FAVORITE } from "./mutations.js";
+export { DELETE_SPEAKER } from "./mutations.js";
+export { ADD_SPEAKER } from "./mutations.js";
