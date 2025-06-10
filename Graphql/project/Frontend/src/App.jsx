bea5fa44d4290bb8b27a04fb2044ef5b800014a8
Petri <dimtri.williams.6448@gmail.com>
2025-06-10 16:53:44 -0500

    Apollo Performance: Setup mutations to add, delete, and toggle a speaker. Setup needed components for adding a speaker.

--- Graphql/project/Frontend/src/App.jsx
+++ Graphql/project/Frontend/src/App.jsx
@@ -1,44 +1,88 @@
 // import "./App.css";
-import { useQuery } from "@apollo/client";
-import { GET_SPEAKERS } from "./graphql";
+import { useQuery, useMutation } from "@apollo/client";
+import { Toolbar } from "./components";
+import {
+  ADD_SPEAKER,
+  DELETE_SPEAKER,
+  GET_SPEAKERS,
+  TOGGLE_SPEAKER_FAVORITE,
+} from "./graphql";
 
 function App() {
   const { data, error, loading } = useQuery(GET_SPEAKERS);
+
+  const [toggleSpeakerFavorite] = useMutation(TOGGLE_SPEAKER_FAVORITE);
+
+  const [deleteSpeaker] = useMutation(DELETE_SPEAKER);
+
+  const [addSpeaker] = useMutation(ADD_SPEAKER);
 
   if (loading) return <div>Loading....</div>;
 
   if (error) return <div>Error: {error.message}</div>;
 
+  const insertSpeakerEvent = (first, last, favorite) => {
+    addSpeaker({
+      variables: {
+        speakerInput: { first, last, favorite },
+      },
+      refetchQueries: [{ query: GET_SPEAKERS }],
+    });
+  };
+
   return (
-    <div className="container show-fav">
-      <div className="row">
-        <div className="fav-list">
-          {data.speakers.datalist.map(({ id, first, last, favorite }) => {
-            return (
-              <div className="favbox" key={id}>
-                <div className="fav-clm col-sm-7">
-                  <h4>
-                    {first} {last} ({id})
-                  </h4>
+    <>
+      <Toolbar insertSpeakerEvent={insertSpeakerEvent} />
+      <div className="container show-fav">
+        <div className="row">
+          <div className="fav-list">
+            {data.speakers.datalist.map(({ id, first, last, favorite }) => {
+              return (
+                <div className="favbox" key={id}>
+                  <div className="fav-clm col-sm-7">
+                    <h4>
+                      {first} {last} ({id})
+                    </h4>
+                  </div>
+                  <div className="fav-clm col-sm-5">
+                    <div className="action">
+                      <span
+                        onClick={() =>
+                          toggleSpeakerFavorite({
+                            variables: {
+                              speakerId: +id || id,
+                            },
+                          })
+                        }
+                      >
+                        <div
+                          className={
+                            favorite === true
+                              ? "fa fa-star orange"
+                              : "fa fa-star-o orange"
+                          }
+                        />
+                        &nbsp;&nbsp; Favorite
+                      </span>
+                      <span
+                        onClick={() =>
+                          deleteSpeaker({
+                            variables: { speakerId: +id || id },
+                            refetchQueries: [{ query: GET_SPEAKERS }],
+                          })
+                        }
+                      >
+                        <i className="fa fa-trash" />
+                      </span>
+                    </div>
+                  </div>
                 </div>
-                <div className="fav-clm col-sm-5">
-                  <span>
-                    <div
-                      className={
-                        favorite === true
-                          ? "fa fa-star orange"
-                          : "fa fa-star-o orange"
-                      }
-                    />
-                    &nbsp;&nbsp; Favorite
-                  </span>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
