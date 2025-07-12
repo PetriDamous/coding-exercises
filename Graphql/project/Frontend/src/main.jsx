2846a49769aa5c8272141667073bc57ab98da048
Petri <dimtri.williams.6448@gmail.com>
2025-07-11 20:05:03 -0500

    Apollo Performance: Install crypto-hash and setup basic persist GET request.

--- Graphql/project/Frontend/src/main.jsx
+++ Graphql/project/Frontend/src/main.jsx
@@ -4,12 +4,24 @@ import { createRoot } from "react-dom/client";
 import "./CSS/style-home.css";
 import "./CSS/style-speakers.css";
 import App from "./App.jsx";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  HttpLink,
+} from "@apollo/client";
+import { createPersistedQueryLink } from "@apollo/client/link/persisted-queries";
 import { inMemoryCacheConfig } from "./graphql/";
+import { sha256 } from "crypto-hash";
+
+const link = createPersistedQueryLink({ sha256 }).concat(
+  new HttpLink({ uri: import.meta.env.VITE_API_URL })
+);
 
 const client = new ApolloClient({
-  uri: import.meta.env.VITE_API_URL,
   cache: new InMemoryCache(inMemoryCacheConfig),
+  link,
+  connectToDevTools: true,
 });
 
 createRoot(document.getElementById("root")).render(
