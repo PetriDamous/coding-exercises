e0979a76078a65507f72f66ba0f7c42e9f141667
Petri <dimtri.williams.6448@gmail.com>
2025-08-23 12:23:58 -0500

    Apollo Subscriptions: Created Subscrtion type and favirote subsciption to schema.

--- Graphql/subscriptions/example/api/resolvers/index.js
+++ Graphql/subscriptions/example/api/resolvers/index.js
@@ -1,8 +1,9 @@
 const Query = require("./query");
 const Mutation = require("./mutation");
 const Session = require("./sessions");
+const Subscription = require("./subscriptions");
 const Speaker = require("./speakers");
 const User = require("./users");
-const resolvers = { Query, Mutation, Session, Speaker, User };
+const resolvers = { Query, Mutation, Session, Subscription, Speaker, User };
 
 module.exports = resolvers;
