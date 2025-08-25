79fe31a34e47683e6d45c9b779907bd0bfbeb013
Petri <dimtri.williams.6448@gmail.com>
2025-08-25 16:41:53 -0500

    Apollo Subscription: Changed variable names in toggleFavoriteSession resolver to avoid confusion on what the object holds.

--- Graphql/subscriptions/example/api/resolvers/subscriptions.js
+++ Graphql/subscriptions/example/api/resolvers/subscriptions.js
@@ -5,5 +5,10 @@ module.exports = {
     subscribe: (parent, args, { pubsub }, info) => {
       return pubsub.asyncIterator([FAVORITE_UPDATE]);
     },
+    // Resolve method to extract the actual data from the payload
+    // Can be used debug or transform the data before sending to client
+    resolve: (payload) => {
+      return payload.favorites;
+    },
   },
 };
