95f05261e6aedb2fd0bb4b1b6b86d0e086db2824
Petri <dimtri.williams.6448@gmail.com>
2025-09-20 14:37:54 -0500

    Delete exmpale folder from subscription project due to change in project plans.

--- Graphql/subscriptions/example/api/resolvers/index.js
+++
@@ -1,9 +0,0 @@
-const Query = require("./query");
-const Mutation = require("./mutation");
-const Session = require("./sessions");
-const Subscription = require("./subscriptions");
-const Speaker = require("./speakers");
-const User = require("./users");
-const resolvers = { Query, Mutation, Session, Subscription, Speaker, User };
-
-module.exports = resolvers;
--- Graphql/subscriptions/example/api/resolvers/utils.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  FAVORITE_UPDATE: "FAVORITE_UPDATE",
-};
