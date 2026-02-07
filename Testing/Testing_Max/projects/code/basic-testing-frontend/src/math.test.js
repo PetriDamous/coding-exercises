e1a928974f2e6d4804f41c8eb42137169fb789d1
Petri <dimtri.williams.6448@gmail.com>
2026-02-07 17:16:30 -0600

    Test_Max: Fixed issue with math.js function that was found during testing. Added more tests to get better coverage for math.js

--- Testing/Testing_Max/projects/code/basic-testing-frontend/src/math.test.js
+++ Testing/Testing_Max/projects/code/basic-testing-frontend/src/math.test.js
@@ -2,7 +2,57 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should add all number values in an array", () => {
-  const result = add([1, 2, 3]);
+  // Arrange
+  const numbers = [1, 2];
 
-  expect(result).toBe(6);
+  const expectedResult = numbers.reduce((acc, curr) => acc + curr, 0);
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResult);
+});
+
+it("should return NaN when when at least one invalide non-numerical string is used", () => {
+  // Arrange
+  const numbers = ["woof", 1];
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+
+  expect(result).toBeNaN();
+});
+
+it("should return correct sum if array of numeric strings are provided", () => {
+  // Arrange
+  const numbers = ["1", "2"];
+
+  const expectedResult = numbers.reduce((acc, curr) => +acc + +curr, 0);
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResult);
+});
+
+it("should return 0 if an empty array is provided", () => {
+  // Arrange
+  const numbers = [];
+  const expectedResult = 0;
+
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  expect(result).toBe(expectedResult);
+});
+
+it("should throw an error when a value is not provided", () => {
+  const resultFn = () => add();
+
+  expect(resultFn).toThrow();
 });
