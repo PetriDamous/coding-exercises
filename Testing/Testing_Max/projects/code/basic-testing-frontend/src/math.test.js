861f3d97ba14d8710525ff6a1b2772393c939014
Petri <dimtri.williams.6448@gmail.com>
2026-02-08 13:17:48 -0600

    Test_Max: Add describe for add function.

--- Testing/Testing_Max/projects/code/basic-testing-frontend/src/math.test.js
+++ Testing/Testing_Max/projects/code/basic-testing-frontend/src/math.test.js
@@ -1,58 +1,60 @@
-import { it, expect } from "vitest";
+import { it, expect, describe } from "vitest";
 import { add } from "./math";
 
-it("should add all number values in an array", () => {
-  // Arrange
-  const numbers = [1, 2];
+describe("add()", () => {
+  it("should add all number values in an array", () => {
+    // Arrange
+    const numbers = [1, 2];
 
-  const expectedResult = numbers.reduce((acc, curr) => acc + curr, 0);
+    const expectedResult = numbers.reduce((acc, curr) => acc + curr, 0);
 
-  // Act
-  const result = add(numbers);
+    // Act
+    const result = add(numbers);
 
-  // Assert
-  expect(result).toBe(expectedResult);
-});
+    // Assert
+    expect(result).toBe(expectedResult);
+  });
 
-it("should return NaN when when at least one invalide non-numerical string is used", () => {
-  // Arrange
-  const numbers = ["woof", 1];
+  it("should return NaN when when at least one invalide non-numerical string is used", () => {
+    // Arrange
+    const numbers = ["woof", 1];
 
-  // Act
-  const result = add(numbers);
+    // Act
+    const result = add(numbers);
 
-  // Assert
+    // Assert
 
-  expect(result).toBeNaN();
-});
+    expect(result).toBeNaN();
+  });
 
-it("should return correct sum if array of numeric strings are provided", () => {
-  // Arrange
-  const numbers = ["1", "2"];
+  it("should return correct sum if array of numeric strings are provided", () => {
+    // Arrange
+    const numbers = ["1", "2"];
 
-  const expectedResult = numbers.reduce((acc, curr) => +acc + +curr, 0);
+    const expectedResult = numbers.reduce((acc, curr) => +acc + +curr, 0);
 
-  // Act
-  const result = add(numbers);
+    // Act
+    const result = add(numbers);
 
-  // Assert
-  expect(result).toBe(expectedResult);
-});
+    // Assert
+    expect(result).toBe(expectedResult);
+  });
 
-it("should return 0 if an empty array is provided", () => {
-  // Arrange
-  const numbers = [];
-  const expectedResult = 0;
+  it("should return 0 if an empty array is provided", () => {
+    // Arrange
+    const numbers = [];
+    const expectedResult = 0;
 
-  // Act
-  const result = add(numbers);
+    // Act
+    const result = add(numbers);
 
-  // Assert
-  expect(result).toBe(expectedResult);
-});
+    // Assert
+    expect(result).toBe(expectedResult);
+  });
 
-it("should throw an error when a value is not provided", () => {
-  const resultFn = () => add();
+  it("should throw an error when a value is not provided", () => {
+    const resultFn = () => add();
 
-  expect(resultFn).toThrow();
+    expect(resultFn).toThrow();
+  });
 });
