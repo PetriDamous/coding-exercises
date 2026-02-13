3c254b9c77eb2492d50bd6e2c5ab60721506936c
Petri <dimtri.williams.6448@gmail.com>
2026-02-12 18:13:24 -0600

    Testing_Max: Add hooks project.

--- Testing/Testing_Max/module_6_async-code-starting-project/async/async-example.test.js
+++ Testing/Testing_Max/module_6_async-code-starting-project/async/async-example.test.js
@@ -1,19 +1,40 @@
-import { expect, it } from "vitest";
-import { generateToken } from "./async-example";
+import { describe, expect, it } from "vitest";
+import { generateToken, generateTokenPromise } from "./async-example";
 
-it("should generate a token value", (done) => {
-  const testUserEmail = "test@test.com";
+describe("generateToken()", () => {
+  it("should generate a token value", (done) => {
+    const testUserEmail = "test@test.com";
 
-  generateToken(testUserEmail, (err, token) => {
-    // expect(token).toBeDefined(); // Currently will pass no matter what
-    // expect(token).toBe(2); // Will also pass no matter what. What is going on here?
+    generateToken(testUserEmail, (err, token) => {
+      // expect(token).toBeDefined(); // Currently will pass no matter what
+      // expect(token).toBe(2); // Will also pass no matter what. What is going on here?
 
-    try {
-      expect(token).toBe(2); // Will now fail like we expect
-      expect(token).toBeDefined(); // Will pass like we expect
-      done(); // Lets vitest know we are done so you can run the test runner
-    } catch (err) {
-      done(err); // Lets vitest know we are done so you can run the test runner
-    }
+      try {
+        // expect(token).toBe(2); // Will now fail like we expect
+        expect(token).toBeDefined(); // Will pass like we expect
+        done(); // Lets vitest know we are done so you can run the test runner
+      } catch (err) {
+        done(err); // Lets vitest know we are done so you can run the test runner
+      }
+    });
+  });
+});
+
+describe("generateTokenPromise()", () => {
+  it("should return a defined value. Using the '.resolver' method.", () => {
+    // Arrange
+    const testEmail = "dogwater@yahoo.com";
+
+    // Assert
+    expect(generateTokenPromise(testEmail)).resolves.toBeDefined();
+  });
+
+  it("should return a defined value. Using 'async await'", async () => {
+    // Arrange
+    const testEmail = "dogwater@yahoo.com";
+    // Act
+    const result = await generateTokenPromise(testEmail);
+    // Assert
+    expect(result).toBeDefined();
   });
 });
